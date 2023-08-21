cbe66d39c4ff4efd393807b9605c3813ee29059c
Alice Parrish <alice@pop-os.localdomain>
2023-08-21 00:55:59 -0400

    First commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react"
+import '../styles/HomePage.scss'
+
+const HomePage: FunctionComponent = () => {
+
+  return (
+    <main>
+      <h1>Home Page</h1>
+    </main>
+  )
+}
+
+export default HomePage
